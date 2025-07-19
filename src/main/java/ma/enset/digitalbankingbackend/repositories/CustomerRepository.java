8395ef43ceb3c35d4e36ebf656286c7ad529e69b
Zakaria MAIT <95222260+MTZACK9@users.noreply.github.com>
2025-07-19 15:37:08 +0100

    ev

--- src/main/java/ma/enset/digitalbankingbackend/repositories/CustomerRepository.java
+++ src/main/java/ma/enset/digitalbankingbackend/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package ma.enset.digitalbankingbackend.repositories;
 import ma.enset.digitalbankingbackend.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByNameContains(String keyword);
 }
